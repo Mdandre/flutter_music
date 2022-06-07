4fb91aa43d03501a54641c9e8e0abf7062696696
Marco DAndrea <dandrea.marco@ecloudsolutions.com>
2022-06-07 08:53:48 -0300

    <feature> ADD player

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,220 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kplayer/kplayer.dart';
 
 void main() {
+  Player.boot();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var player = Player.network("https://archive.org/download/s0v5kiwcwp1zaps7pdwjtp5o8e6clmevdnewngnd/JSJ_385_Panel.mp3", autoPlay: false);
+  @override
+  void initState() {
+    super.initState();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  bool _changingPosition = false;
+  bool loading = false;
+  Duration _position = Duration.zero;
+  double get position {
+    if (player.duration.inSeconds == 0) {
+      return 0;
+    }
+    if (_changingPosition) {
+      return _position.inSeconds * 100 / player.duration.inSeconds;
+    } else {
+      return player.position.inSeconds * 100 / player.duration.inSeconds;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return CupertinoApp(
+      color: Colors.teal,
+      theme: const CupertinoThemeData(
+        primaryColor: Colors.teal,
+        primaryContrastingColor: Colors.teal,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: StreamBuilder(
+              stream: player.streams.position,
+              builder: (context, snapshot) {
+                return Text(player.status.toString());
+              }),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: StreamBuilder(
+                      stream: player.positionStream,
+                      builder: (context, snapshot) {
+                        return CircularProgressIndicator(
+                          strokeWidth: 1,
+                          color: Colors.teal,
+                          value: loading ? null : player.position.inSeconds / max(player.duration.inSeconds, 0.01),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    height: 150,
+                    child: FloatingActionButton(
+                      disabledElevation: 0,
+                      elevation: 0,
+                      focusElevation: 0,
+                      hoverElevation: 0,
+                      highlightElevation: 0,
+                      backgroundColor: Colors.teal.withOpacity(0.1),
+                      onPressed: () {
+                        setState(() {
+                          player.toggle();
+                        });
+                      },
+                      child: DefaultTextStyle(
+                        style: const TextStyle(color: Colors.black87),
+                        child: StreamBuilder(
+                          stream: player.positionStream,
+                          builder: (context, snapshot) {
+                            return player.playing
+                                ? const Icon(
+                                    Icons.pause,
+                                    color: Colors.teal,
+                                    size: 80,
+                                  )
+                                : const Icon(
+                                    Icons.play_arrow,
+                                    color: Colors.teal,
+                                    size: 80,
+                                  );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                width: double.infinity,
+                child: StreamBuilder(
+                    stream: player.streams.position,
+                    builder: (context, AsyncSnapshot<Duration> snapshot) {
+                      loading = false;
+                      return CupertinoSlider(
+                        value: position,
+                        min: 0,
+                        max: 100,
+                        onChangeStart: (double value) {
+                          _changingPosition = true;
+                        },
+                        onChangeEnd: (double value) {
+                          _changingPosition = false;
+                        },
+                        onChanged: (double value) {
+                          setState(() {
+                            loading = true;
+                            _position = Duration(seconds: ((value / 100) * player.duration.inSeconds).toInt());
+                            player.position = _position;
+                          });
+                        },
+                      );
+                    }),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const Icon(Icons.volume_up),
+                    Expanded(
+                      child: StreamBuilder(
+                        stream: player.streams.position,
+                        builder: (context, AsyncSnapshot<Duration> snapshot) {
+                          return CupertinoSlider(
+                            value: player.volume * 100,
+                            min: 0,
+                            max: 100,
+                            // label: player.volume.round().toString(),
+                            onChanged: (double value) {
+                              setState(() {
+                                player.volume = value / 100;
+                              });
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    const Icon(Icons.speed),
+                    Expanded(
+                      child: StreamBuilder(
+                        stream: player.streams.position,
+                        builder: (context, AsyncSnapshot<Duration> snapshot) {
+                          return CupertinoSlider(
+                            value: player.speed * 10,
+                            min: 0,
+                            max: 10,
+                            divisions: 10,
+                            onChanged: (double value) {
+                              setState(() {
+                                player.speed = value / 10;
+                              });
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: DefaultTextStyle(
+                  style: TextStyle(color: Colors.black38, fontSize: 12),
+                  child: Column(
+                    children: [
+                      Text("Duration: " + player.duration.toString()),
+                      Text("Position: " + player.position.toString()),
+                      Text("Volume: " + player.volume.toString()),
+                      Text("Speed: " + player.speed.toString()),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
